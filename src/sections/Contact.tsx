f04d356d808664409820a420ce7fa6b01f23fb09
Jan Steinhoff <jan.steinhoff@sinnerschrader.com>
2019-05-21 20:24:27 +0200

    add phone contact field

--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -1,55 +1,65 @@
-import React from 'react'
+import React from 'react';
 import {
-	FormattedMessage as Translated,
-	injectIntl,
-	InjectedIntlProps,
-} from 'react-intl'
+  FormattedMessage as Translated,
+  injectIntl,
+  InjectedIntlProps
+} from 'react-intl';
 
-import styles from './contact.module.scss'
-import PrimaryButton from '../components/PrimaryButton'
-import SectionLayout from '../layout/Section'
-import { CONTACT_EMAIL_ADDRESS } from '../config'
+import styles from './contact.module.scss';
+import PrimaryButton from '../components/PrimaryButton';
+import SectionLayout from '../layout/Section';
+import {CONTACT_EMAIL_ADDRESS} from '../config';
 
 interface ContactProps extends InjectedIntlProps {}
 
 const Contact: React.FunctionComponent<ContactProps> = ({
-	intl: { formatMessage },
+  intl: {formatMessage}
 }: ContactProps) => (
-	<SectionLayout id="contact">
-		<form
-			action={`https://formspree.io/${CONTACT_EMAIL_ADDRESS}`}
-			method="POST"
-		>
-			<h1>
-				<Translated id="contact-header" />
-			</h1>
-			<div>
-				<div className={styles.formGroup}>
-					<input type="email" name="email" required placeholder="email" />
-					<label htmlFor="email">email</label>
-				</div>
-				<div className={styles.formGroup}>
-					<input name="name" required placeholder="name" />
-					<label htmlFor="name">name</label>
-				</div>
-			</div>
-			<div className={styles.formGroup}>
-				<textarea
-					name="message"
-					required
-					placeholder={formatMessage({ id: 'message-label' })}
-				/>
-				<label htmlFor="message">
-					<Translated id="message-label" />
-				</label>
-			</div>
-			<div className={styles.formGroup}>
-				<PrimaryButton type="submit">
-					<Translated id="send-button-text" />
-				</PrimaryButton>
-			</div>
-		</form>
-	</SectionLayout>
-)
+  <SectionLayout id="contact">
+    <form
+      action={`https://formspree.io/${CONTACT_EMAIL_ADDRESS}`}
+      method="POST"
+    >
+      <h1>
+        <Translated id="contact-header" />
+      </h1>
+      <div>
+        <div className={styles.formGroup}>
+          <input name="name" required placeholder="name" />
+          <label htmlFor="name">name</label>
+        </div>
+        <div className={styles.formGroup}>
+          <input type="email" name="email" required placeholder="email" />
+          <label htmlFor="email">email</label>
+        </div>
+        <div className={styles.formGroup}>
+          <input
+            name="phone"
+            required
+            placeholder={formatMessage({id: 'phone-label'})}
+          />
+          <label htmlFor="phone">
+            <Translated id="phone-label" />
+          </label>
+        </div>
+      </div>
+      <div className={styles.formGroup}>
+        <textarea
+          name="message"
+          required
+          placeholder={formatMessage({id: 'message-label'})}
+        />
+        <label htmlFor="message">
+          <Translated id="message-label" />
+        </label>
+      </div>
+      <div className={styles.formGroup}>
+        <PrimaryButton type="submit">
+          <Translated id="send-button-text" />
+        </PrimaryButton>
+      </div>
+    </form>
+  </SectionLayout>
+);
 
-export default injectIntl(Contact)
+export default injectIntl(Contact);
